b3053eb90f5e56a5ac3f93bd50fdb0bc8e4f26aa
WongQi <wongql-wp16@student.tarc.edu.my>
2020-08-24 17:37:30 +0800

    fixed bug where index wont redirect to login if no user

--- Pages/index.php
+++ Pages/index.php
@@ -2,6 +2,10 @@
 <?php
 require_once '../SecurityClasses/DatabaseConnection.php';
 require_once '../class/Returns.php';
+require_once '../SecurityClasses/SessionManagement.php';
+
+require_once '../SecurityClasses/AccessControl.php';
+require_once '../class/Rental.php';
 ?>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,34 +15,56 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-    </head>
+        <title>Index</title>
 
+    </head>
+    <style>
+        .btn-group input {
+
+            border: 1px solid ; /* Green border */
+
+            padding: 10px 24px; /* Some padding */
+            margin:10px auto; 
+            height: 100px;
+            size: 40px;
+            cursor: pointer; /* Pointer/hand icon */
+            width: 50%; /* Set a width if needed */
+            display: block; /* Make the buttons appear below each other */
+        }
+
+        .btn-group  input :not(:last-child) {
+            border-bottom: none; /* Prevent double borders */
+        }
+
+        /* Add a background color on hover */
+        .btn-group  input :hover {
+            background-color: #3e8e41;
+        }
+    </style>
     <body>
         <?php
-        require_once '../SecurityClasses/SessionManagement.php';
-         require_once '../SecurityClasses/DatabaseConnection.php';
-         require_once '../SecurityClasses/AccessControl.php';
-         require_once '../class/Rental.php';
-       
-         AccessControl::checkUser();
-     
+        
+        AccessControl::checkUser();
+         
+
         if (array_key_exists('logout', $_POST)) {
             SessionManagement::Logout();
         }
-      if (array_key_exists('Rent', $_POST)) {
-      $db= DatabaseConnection::getInstance();
-          $db->createBookXMLFile();
-          $db->closeConnection();
-          
+        if (array_key_exists('Rent', $_POST)) {
+            $db = DatabaseConnection::getInstance();
+            $db->createBookXMLFile();
+            $db->closeConnection();
+
             header("Location: rent.php");
         }
         ?>
-        <form method="POST" action="index.php">
-            <input type="submit" value="Rent"name="Rent" onClick="document.location.href = 'rent.php'" />
-            <input type="button" value="return" onClick="document.location.href = 'return.php'" />
-            <input type="submit" value="logout" name="logout" />
-            <?php AccessControl::LoadStaffControl();?>
+        <form method="POST" action="index.php" >
+            <div class="btn-group">
+                <input type="submit"value="Rent"name="Rent" onClick="document.location.href = 'rent.php'" />
+                <input type="button" value="return" onClick="document.location.href = 'return.php'" />
+                <input type="submit"  value="logout" name="logout" />
+                <?php AccessControl::LoadStaffControl(); ?>
+            </div>
         </form> 
 
 
--- SecurityClasses/AccessControl.php
+++ SecurityClasses/AccessControl.php
@@ -1,11 +1,12 @@
 <?php
 
 class AccessControl {
-    static public function checkUser(){
-       session_start();
-       if(empty($_SESSION['user'])){
-           header("Location : login.php");
-       }
+
+    static public function checkUser() {
+        session_start();
+        if (empty($_SESSION['user'])) {
+            header('Location: login.php');
+        }
     }
 
     static public function LoadStaffControl() {
@@ -15,7 +16,7 @@ class AccessControl {
             echo " <input type='button' value='addNewBook' "
             . "onClick=\"document.location.href = 'AddNewBook.php'\" />"
             ;
-                        echo " <input type='button' value='UserList' "
+            echo " <input type='button' value='UserList' "
             . "onClick=\"document.location.href = 'UserList.php'\" />"
             ;
         }
